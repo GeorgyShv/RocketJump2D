364633f15ef5a4e1946465aa7a328786d7e7e54a
GeorgyShv <mr.gosha9@mail.ru>
2023-10-27 19:25:30 +0300

    added inputs classes: MouseInputs and KeyboardInputs

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,20 +1,32 @@
 package main;
 
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class GamePanel extends JPanel {
 
+    private MouseInputs mouseInputs;
     public GamePanel() {
+
+        mouseInputs = new MouseInputs();
+        addKeyListener(new KeyboardInputs());
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
 
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(new Color(0, 1, 0));
+        g.setColor(new Color(230, 90, 10));
         g.fillRect(1, 10, 200, 50);
-        g.setColor(new Color(200, 80, 30));
-        g.fillOval(50, 50, 100, 20);
+
     }
 }
